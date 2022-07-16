5495e6ed55b76e935cdccc38c133b2e077d542a4
Luiz Fernando <lfernando488@gmail.com>
2022-07-16 16:02:25 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/NotificationIcon.svg';
 import { BASE_URL } from '../../utils/request';
 import '../NotificationButton/styles.css';
@@ -9,7 +10,7 @@ type Props ={
 
 function handleClick(id: number){
     axios(`${BASE_URL}/sales/${id}/notification`)
-    .then(response =>{})
+    .then(response =>{toast.info("Sms enviado!")})
     .catch(error => console.log(error))
 }
 
